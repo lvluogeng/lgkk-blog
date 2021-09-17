57378bc1207b59ef3e6b8e7049de9c6e353299eb
lvluogeng <34018827+lvluogeng@users.noreply.github.com>
2021-09-17 21:53:23 +0800

    Add files via upload

---
+++ docs/config.js
@@ -0,0 +1,20 @@
+const head = require('./config/head.js');
+const plugins = require('./config/plugins.js');
+const themeConfig = require('./config/themeConfig.js');
+
+module.exports = {
+  theme: 'vdoing', // 使用npm包主题
+  // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
+
+  title: "LGKK's blog",
+  description: '小菜鸡的学习笔记[.^.]，天上风光壮，人间浩气长。',
+  port: 2020,
+  base: '/lgkk-blog/', // 格式：'/<仓库名>/'， 默认'/'
+  markdown: {
+    lineNumbers: true, // 代码行号
+  },
+
+  head,
+  plugins,
+  themeConfig,
+}
